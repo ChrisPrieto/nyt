7d15d01817bc503bdd303d54644a6fac3ab43d33
Chris Prieto <chris_prieto@msn.com>
2018-09-04 01:10:35 -0500

    Update Footer.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
         <footer className="page-footer footer-copyright grey darken-4">
             <div className="container">
                 <div className="center-align">
-                    Copyright © 2017 by <a href="https://www.linkedin.com" target="_blank" className="teal-text text-lighten-5" rel="noopener noreferrer">Chris Prieto</a>, all rights reserved.
+                    Copyright © 2018 by <a href="https://www.linkedin.com" target="_blank" className="teal-text text-lighten-5" rel="noopener noreferrer">Chris Prieto</a>, all rights reserved.
 
                     <div className="nyt-separator-2"></div>
                 </div>
